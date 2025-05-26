e0ede73c762a49b4ea6df30c04509388b45c368f
Axel Quidem <quidemjohnaxel@gmail.com>
2025-05-26 20:18:26 +0800

    Add some blocks of code for this branch

--- Activity1/Activity1.cs
+++ Activity1/Activity1.cs
@@ -21,6 +21,17 @@ class Program
         // Calling a function that returns a boolean
         bool isTrue = IsTrue(age);
         Console.WriteLine("Is he/she legal age? " + isTrue);
+
+        // New functions added in this branch
+        DisplayQuote();
+        string reversed = ReverseString("Hello C#!");
+        Console.WriteLine("Reversed String: " + reversed);
+
+        int sum = AddNumbers(10, 20, 30);
+        Console.WriteLine("Sum of numbers: " + sum);
+
+        string status = AgeStatus(age);
+        Console.WriteLine("Age Status: " + status);
     }
 
     static void createBranch()
@@ -29,27 +40,57 @@ class Program
         Console.WriteLine("Branch created successfully!");
     }
 
-    // Function with no parameters and no return value
     static void HelloWorld()
     {
-        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello, C#! Updated from feature branch.");
     }
 
-    // Function with parameters and no return value
     static void Introduce(string name)
     {
-        Console.WriteLine("I'm " + name + "!");
+        Console.WriteLine("Nice to meet you! I'm " + name + " from the feature branch!");
     }
 
-    // Function with parameters and a return value
-    static int ageCalcu(int a, int b)
+    static int ageCalcu(int currentYear, int birthYear)
     {
-        return a - b;
+        return currentYear - birthYear;
     }
 
-    // Function returning a boolean value
     static bool IsTrue(int number)
     {
         return number >= 18;
     }
+
+    // New function: Display a motivational quote
+    static void DisplayQuote()
+    {
+        Console.WriteLine("\"The only way to do great work is to love what you do.\" - Steve Jobs");
+    }
+
+    // New function: Reverse a string
+    static string ReverseString(string input)
+    {
+        char[] charArray = input.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+
+    // New function: Add multiple numbers
+    static int AddNumbers(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+
+    // New function: Return status based on age
+    static string AgeStatus(int age)
+    {
+        if (age < 13)
+            return "Child";
+        else if (age < 20)
+            return "Teenager";
+        else if (age < 60)
+            return "Adult";
+        else
+            return "Senior";
+    }
 }
+
